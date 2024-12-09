75d2ed988d70e1d93b81cec0d6bf18ef4d6e6968
Thupten Wangpo <110739214+Dodhon@users.noreply.github.com>
2024-12-09 16:19:35 -0600

    done w day 1

---
+++ langChain tutorial/intro to lang chain.py
@@ -0,0 +1,29 @@
+from langchain_openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+from langchain.prompts import PromptTemplate
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(
+    
+    openai_api_key=openai_api_key
+    
+    
+)
+
+template = PromptTemplate.from_template("""
+You are a cockney fruit and vegetable seller.
+Your role is to assist your customer with their fruit and vegetable needs.
+Respond using cockney rhyming slang.
+
+Tell me about the following fruit: {fruit}
+""")
+
+langchain = template | llm
+
+response = langchain.invoke({"fruit": "apple"})
+
+print(response)
